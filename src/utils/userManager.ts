ae692e3f2a7832c919180c68ca4046c17459d317
John Morgan <johnsmorgan@me.com>
2020-10-08 19:32:45 -0400

    fix: fixes #29 signout not working as intended

--- src/utils/userManager.ts
+++ src/utils/userManager.ts
@@ -4,16 +4,22 @@ export interface IMockUserManagerOptions extends UserManagerSettings {
   getUserFunction: () => Promise<any>
   signinRedirectCallback: () => Promise<any>
   signinRedirectFunction: () => void
+  signoutRedirectFunction: () => void
+  signoutRedirectCallback: () => Promise<any>
 }
 class UserManager {
   getUserFunction: () => Promise<any>
   signinRedirectCallbackFunction: () => Promise<any>
   signinRedirectFunction: () => void
+  signoutRedirectFunction: () => void
+  signoutRedirectCallbackFunction: () => Promise<any>
 
   constructor(args: IMockUserManagerOptions) {
     this.getUserFunction = args.getUserFunction
     this.signinRedirectFunction = args.signinRedirectFunction
     this.signinRedirectCallbackFunction = args.signinRedirectCallback
+    this.signoutRedirectFunction = args.signoutRedirectFunction
+    this.signoutRedirectCallbackFunction = args.signoutRedirectCallback
   }
 
   getUser() {
@@ -31,6 +37,17 @@ class UserManager {
     return this.signinRedirectCallbackFunction
       ? this.signinRedirectCallbackFunction()
       : new Promise(res => res())
+  }
+
+  signoutRedirect(): void {
+    return this.signoutRedirectFunction
+      ? this.signoutRedirectFunction()
+      : undefined;
+  }
+  signoutRedirectCallback() {
+    return this.signoutRedirectCallbackFunction
+      ? this.signoutRedirectCallbackFunction()
+      : undefined;
   }
 }
 
